6fb7b1a90764ebf00a73d6dee9a2ca909fd7eb2e
Xavier Fernando <xavier.r.fernando@hotmail.com>
2020-06-04 12:23:14 +0530

    till 10

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace NetFrameworkWebAPI
@@ -20,12 +22,39 @@ namespace NetFrameworkWebAPI
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            //DIFFERENT FORMATTING
+
             //Indent JSON data
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = 
-                Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
             //Camel case instead of Pascal case
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = 
-                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            ////Return ONLY json
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            ////Return ONLY xml
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            //Returns json in all browsers
+            //The problem with this approach is that Content - Type header of the response is set to text / html which is misleading.
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            ////Calling Custom Json Formatter : Problem in above approach is overcome
+            //config.Formatters.Add(new CustomJsonFormatter());
+        }
+
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
         }
     }
 }
--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -11,7 +11,8 @@ namespace NetFrameworkWebAPI.Controllers
     public class EmployeeController : ApiController
     {
         //Employee & Employees are auto-generated using EF
-        public IEnumerable<Employee> Get()
+        [HttpGet]
+        public IEnumerable<Employee> FetchEmployees()
         {
             using (DevDBEntities entities = new DevDBEntities())
             {
@@ -19,11 +20,96 @@ namespace NetFrameworkWebAPI.Controllers
             }
         }
 
-        public Employee Get(int id)
+        [HttpGet]
+        public HttpResponseMessage FetchEmployeeById(int id)
         {
             using (DevDBEntities entities = new DevDBEntities())
             {
-                return entities.Employees.FirstOrDefault(e => e.ID == id);
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee " + id.ToString() +  " does not exist");    
+                }
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddEmployee(Employee employee)
+        {
+            try
+            {
+                using (DevDBEntities entities = new DevDBEntities())
+                {
+                    var newemp_entity = entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.Created, newemp_entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+                using (DevDBEntities entities = new DevDBEntities())
+                {
+                    var newemp_entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (newemp_entity != null)
+                    {
+                        newemp_entity.FirstName = employee.FirstName;
+                        newemp_entity.LastName = employee.LastName;
+                        newemp_entity.Gender = employee.Gender;
+                        newemp_entity.Salary = employee.Salary;
+
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, newemp_entity);
+                    }
+                    else 
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Emp Id " + id.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage DeleteEmployee(int id)
+        {
+            try
+            {
+                using (DevDBEntities entities = new DevDBEntities())
+                {
+                    var deleteEmp_entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (deleteEmp_entity != null)
+                    {
+                        entities.Employees.Remove(entities.Employees.FirstOrDefault(e => e.ID == id));
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, deleteEmp_entity);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Emp ID " + id.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
